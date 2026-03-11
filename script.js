a780a0152c3365cc32a77fbdada2d26728722f5b
Zenoxx-Tech <voltgamiingshow@gmail.com>
2026-03-11 14:47:52 +0100

    u

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
 function twitch(){
-window.open("https://twitch.tv/byzenoxx")
+
+window.open("https://twitch.tv/ByZenoxxx")
+
 }
 
 function tiktok(){
-window.open("https://tiktok.com/@byzenoxx")
-}
 
-async function checkLive(){
-try{
-let response = await fetch("https://api.twitch.tv/helix/streams?user_login=byzenoxx", {
-headers:{
-'Client-ID':'TON_CLIENT_ID',
-'Authorization':'Bearer TON_ACCESS_TOKEN'
-}
-})
-let data = await response.json()
-if(data.data.length>0){
-document.getElementById("live-status").innerText="🔴 LIVE"
-}else{
-document.getElementById("live-status").innerText="⚫ Offline"
-}
-}catch{
-document.getElementById("live-status").innerText="⚫ Offline"
-}
-}
+window.open("https://tiktok.com/@ByZenoxxx")
 
-checkLive()
+}
