43802445cdaa167d1e39c2317ab37000f2b1eb1b
Zenoxx-Tech <voltgamiingshow@gmail.com>
2026-03-11 03:27:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-window.onload = function(){
-
-console.log("Site ByZenoxx chargé 🚀");
-
+function twitch(){
+window.open("https://twitch.tv/byzenoxx")
 }
 
-document.querySelectorAll("nav a").forEach(link=>{
+function tiktok(){
+window.open("https://tiktok.com/@byzenoxx")
+}
 
-link.addEventListener("click", function(e){
-
-e.preventDefault()
-
-document.querySelector(this.getAttribute("href")).scrollIntoView({
-
-behavior:"smooth"
-
+async function checkLive(){
+try{
+let response = await fetch("https://api.twitch.tv/helix/streams?user_login=byzenoxx", {
+headers:{
+'Client-ID':'TON_CLIENT_ID',
+'Authorization':'Bearer TON_ACCESS_TOKEN'
+}
 })
+let data = await response.json()
+if(data.data.length>0){
+document.getElementById("live-status").innerText="🔴 LIVE"
+}else{
+document.getElementById("live-status").innerText="⚫ Offline"
+}
+}catch{
+document.getElementById("live-status").innerText="⚫ Offline"
+}
+}
 
-})
-
-})
+checkLive()
